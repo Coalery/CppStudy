5bfb81f427d689df6e6bf23aa4757543509c68c5
Coalery <doralife12@naver.com>
2020-03-08 17:54:08 +0900

    005

---
+++ _005.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+// Operator
+
+using namespace std;
+
+int main() {
+    int a = -1;
+    int b = -1;
+
+    cin >> a;
+    cin >> b;
+
+    cout << a + b << endl;
+    cout << a - b << endl;
+    cout << a * b << endl;
+    cout << a / b << endl;
+    cout << a % b << endl;
+
+    int num1 = 2;
+    int num2 = 5;
+    int num3 = num1 + num2;
+    int num4 = 6;
+    num4 += 7;
+    
+    cout << num3 << ", " << num4 << endl;
+
+    num3++;
+    num4--;
+
+    cout << num3 << ", " << num4 << endl;
+
+    cout << (num1 != num2) << endl;
+    cout << (num1 > num2) << endl;
+    
+    return 0;
+}
